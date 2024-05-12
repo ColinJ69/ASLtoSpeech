1209da1c159ff29e5eccee97179ba81f12fc4049
Colin J <johnstoncolin394@gmail.com>
2024-05-12 12:55:58 -0400

    Update and rename download_train_model.ipynb to download_train_model.py

--- download_train_model.py
+++ download_train_model.py
@@ -10,3 +10,4 @@ dataset = version.download("yolov8")
 data = f'{dataset.location}/data.yaml'
 
 !yolo task=detect mode=train model=yolov8s.pt data={dataset.location}/data.yaml epochs=2 imgsz=640
+#I ran this code in google colab but idk how to put jupyter notebook on github nor do I want to learn how to.
